6b3b9ef5c284083f50ec12ae3af327faae1c625c
avanegas <arco527@gmail.com>
2025-04-07 17:52:57 -0500

    Add basic traffic light logic

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,91 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [color, setColor] = useState("red");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleClickColor = () => {
+    if (color === "red") {
+      setColor("yellow");
+    } else if (color === "yellow") {
+      setColor("green");
+    } else if (color === "green") {
+      setColor("blue");
+    } else {
+      setColor("red");
+    }
+  };
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center flex-column"
+      style={{
+        height: "100vh",
+        backgroundColor: "black",
+        color: "white",
+      }}
+    >
+      <h1 className="text-center mt-5">Traffic Light</h1>
+
+      <div className="d-flex flex-column align-items-center mt-4">
+        <div
+          style={{
+            backgroundColor: "black",
+            borderRadius: "10px",
+            padding: "10px",
+            border: "2px solid white",
+          }}
+        >
+          <div
+            className="rounded-circle mb-2"
+            style={{
+              width: "30px",
+              height: "30px",
+              backgroundColor: "red",
+              opacity: color === "red" ? 1 : 0.3,
+            }}
+          ></div>
+          <div
+            className="rounded-circle mb-2"
+            style={{
+              width: "30px",
+              height: "30px",
+              backgroundColor: "yellow",
+              opacity: color === "yellow" ? 1 : 0.3,
+            }}
+          ></div>
+          <div
+            className="rounded-circle"
+            style={{
+              width: "30px",
+              height: "30px",
+              backgroundColor: "green",
+              opacity: color === "green" ? 1 : 0.3,
+            }}
+          ></div>
+          <div
+            className="rounded-circle mb-2"
+            style={{
+              width: "30px",
+              height: "30px",
+              backgroundColor: "blue",
+              opacity: color === "blue" ? 1 : 0.3,
+            }}
+          ></div>
+        </div>
+      </div>
+      <a
+        onClick={handleClickColor}
+        className="btn btn-success"
+        style={{ margin: "50px" }}
+      >
+        Click me!
+      </a>
+      <p>
+        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+        love!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
